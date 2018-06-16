c4216df3648c87bb386e4c2f506f796831d49e15
wuchao <wuchao900726@gmail.com>
2018-06-16 05:13:54 +0000

    Setting up the Sitecore Rocks Connection.

---
+++ events.tac.local.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
